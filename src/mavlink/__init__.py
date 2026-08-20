e9f9cedf7627f31a119a12f1155f28e7e98062ba
Yuta Suzuki <suzuki7yuta@gmail.com>
2026-08-20 19:07:32 +0900

    mavlink_mapを外部に公開した

--- src/mavlink/__init__.py
+++ src/mavlink/__init__.py
@@ -1,7 +1,8 @@
 from .generated import mavlink
+from .generated.mavlink import mavlink_map
 from .data import MAVLinkData
 from .subscriber import MAVLinkSubscriber
 from .history import MAVLinkHistory
 from .status import MAVLinkStatus
 
-__all__ = ["mavlink", "MAVLinkData", "MAVLinkSubscriber", "MAVLinkHistory"]
+__all__ = ["mavlink", "mavlink_map", "MAVLinkData", "MAVLinkSubscriber", "MAVLinkHistory", "MAVLinkStatus"]
